2590fbc1ac94eec33e1dcae1fe2e25ebde55b45f
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-02 16:04:09 +0200

    Changed table

--- components/CowList.vue
+++ components/CowList.vue
@@ -1,25 +1,29 @@
 <template>
   <h1>Cowlist</h1>
   <div>
-    <table class="border-collapse border border-gray-400 ...">
-      <thead>
+    <table class="w-full border-collapse border border-gray-400 bg-white text-sm dark:border-gray-500 dark:bg-gray-800">
+      <thead class="bg-gray-50 dark:bg-gray-700">
         <tr>
-          <th class="border border-gray-300 ...">State</th>
-          <th class="border border-gray-300 ...">City</th>
+          <th
+            class="w-1/2 border border-gray-300 p-4 text-left font-semibold text-gray-900 dark:border-gray-600 dark:text-gray-200">
+            State</th>
+          <th
+            class="w-1/2 border border-gray-300 p-4 text-left font-semibold text-gray-900 dark:border-gray-600 dark:text-gray-200">
+            City</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td class="border border-gray-300 ...">Indiana</td>
-          <td class="border border-gray-300 ...">Indianapolis</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Indiana</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Indianapolis</td>
         </tr>
         <tr>
-          <td class="border border-gray-300 ...">Ohio</td>
-          <td class="border border-gray-300 ...">Columbus</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Ohio</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Columbus</td>
         </tr>
         <tr>
-          <td class="border border-gray-300 ...">Michigan</td>
-          <td class="border border-gray-300 ...">Detroit</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Michigan</td>
+          <td class="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400">Detroit</td>
         </tr>
       </tbody>
     </table>
